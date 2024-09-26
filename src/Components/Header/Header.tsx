fb8bf7e237f30c7dbbf20017524af7ba59787578
walter10x <wrivero1403@gmail.com>
2024-09-26 12:32:40 +0200

    refactory Header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -9,18 +9,18 @@ export const Header: React.FC = () => {
         <div className="flex items-center justify-between h-16">
           {/* Logo o Título */}
           <div className="flex-shrink-0">
-            <h1 className="text-2xl font-bold text-white">Dashboard</h1>
+            <h1 className="text-2xl font-bold text-white">Sistema De Control</h1>
           </div>
 
-          {/* Enlaces de navegación */}
-          <nav className="hidden md:flex space-x-8">
+         
+          {/* <nav className="hidden md:flex space-x-8">
             <Link to="/login" className="text-gray-300 hover:text-white transition duration-300 ease-in-out">
               Login
             </Link>
             <Link to="/register" className="text-gray-300 hover:text-white transition duration-300 ease-in-out">
               Añadir Usuario
             </Link>
-          </nav>
+          </nav> */}
 
           {/* Bienvenida al usuario */}
           <div className="flex items-center">
